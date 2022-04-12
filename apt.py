4c22894e7fb4f9ec8f9250f4a5b3527423ed69f3
seolhokim <kilmya1@naver.com>
2022-04-12 12:29:05 +0900

    first commit

---
+++ apt.py
@@ -0,0 +1,156 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import copy
+import math
+
+import utils
+import hydra
+
+from drq import DRQAgent
+
+class RandomShiftsAug(nn.Module):
+    def __init__(self, pad):
+        super().__init__()
+        self.pad = pad
+
+    def forward(self, x):
+        n, c, h, w = x.size()
+        assert h == w
+        padding = tuple([self.pad] * 4)
+        x = F.pad(x, padding, 'replicate')
+        eps = 1.0 / (h + 2 * self.pad)
+        arange = torch.linspace(-1.0 + eps,
+                                1.0 - eps,
+                                h + 2 * self.pad,
+                                device=x.device,
+                                dtype=x.dtype)[:h]
+        arange = arange.unsqueeze(0).repeat(h, 1).unsqueeze(2)
+        base_grid = torch.cat([arange, arange.transpose(1, 0)], dim=2)
+        base_grid = base_grid.unsqueeze(0).repeat(n, 1, 1, 1)
+
+        shift = torch.randint(0,
+                              2 * self.pad + 1,
+                              size=(n, 1, 1, 2),
+                              device=x.device,
+                              dtype=x.dtype)
+        shift *= 2.0 / (h + 2 * self.pad)
+
+        grid = base_grid + shift
+        return F.grid_sample(x,
+                             grid,
+                             padding_mode='zeros',
+                             align_corners=False)
+    
+class APTAgent(DRQAgent):
+    def __init__(self, reward_free, knn_clip, knn_k, knn_avg, knn_rms, lr, **kwargs):
+        super().__init__(lr = lr, **kwargs)
+        self.reward_free = reward_free
+        self.projection = nn.Sequential(
+            nn.Linear(50, 128),
+            nn.ReLU(inplace=True),
+            nn.Linear(128, 64),
+            nn.LayerNorm(64),
+        ).to(self.device)
+        
+        self.projection.train()
+        
+        self.projection_optimizer = torch.optim.Adam(self.projection.parameters(), lr=lr)
+        
+        #contrastive learning
+        self.cross_entropy_loss = nn.CrossEntropyLoss()
+        rms = utils.RMS(self.device)
+        self.pbe = utils.PBE(rms, knn_clip, knn_k, knn_avg, knn_rms,
+                             self.device)
+        
+    def compute_reward(self, next_obs):
+        representation = self.critic.encoder(next_obs)
+        reward = self.pbe(representation).reshape(-1,1)
+        return reward
+    
+    def update_representation(self, obs, logger, step):
+        anchor = self.aug(obs)
+        anchor_rep = self.critic.get_representation(anchor)
+        anchor_proj = self.projection(torch.relu(anchor_rep))
+        
+        positive = self.aug(obs)
+        positive_rep = self.critic.get_representation(positive)
+        positive_proj = self.projection(torch.relu(positive_rep)).detach() #curl        
+        
+        logits = torch.matmul(anchor_proj, positive_proj.T)
+        labels = torch.arange(logits.shape[0]).long().to(self.device)
+        loss = self.cross_entropy_loss(logits, labels)
+        
+        self.critic_optimizer.zero_grad()
+        self.projection_optimizer.zero_grad()
+        loss.backward()
+        self.critic_optimizer.step()
+        self.projection_optimizer.step()
+        logger.log('train/nce_loss', loss, step)
+    def update_critic(self, obs, obs_aug, action, reward, next_obs,
+                      next_obs_aug, not_done, logger, step):
+        with torch.no_grad():
+            dist = self.actor(next_obs)
+            next_action = dist.rsample()
+            log_prob = dist.log_prob(next_action).sum(-1, keepdim=True)
+            target_Q1, target_Q2 = self.critic_target(next_obs, next_action)
+            target_V = torch.min(target_Q1,
+                                 target_Q2) - self.alpha.detach() * log_prob
+            target_Q = reward + (not_done * self.discount * target_V)
+
+            dist_aug = self.actor(next_obs_aug)
+            next_action_aug = dist_aug.rsample()
+            log_prob_aug = dist_aug.log_prob(next_action_aug).sum(-1,
+                                                                  keepdim=True)
+            target_Q1, target_Q2 = self.critic_target(next_obs_aug,
+                                                      next_action_aug)
+            target_V = torch.min(
+                target_Q1, target_Q2) - self.alpha.detach() * log_prob_aug
+            target_Q_aug = reward + (not_done * self.discount * target_V)
+
+            target_Q = (target_Q + target_Q_aug) / 2
+
+        # get current Q estimates
+        current_Q1, current_Q2 = self.critic(obs, action, detach_encoder = True) #detach_encoder = True
+        critic_loss = F.mse_loss(current_Q1, target_Q) + F.mse_loss(
+            current_Q2, target_Q)
+
+        Q1_aug, Q2_aug = self.critic(obs_aug, action, detach_encoder = True) #####
+
+        critic_loss += F.mse_loss(Q1_aug, target_Q) + F.mse_loss(
+            Q2_aug, target_Q)
+
+        logger.log('train_critic/loss', critic_loss, step)
+
+        # Optimize the critic
+        self.critic_optimizer.zero_grad()
+        critic_loss.backward()
+        self.critic_optimizer.step()
+
+        self.critic.log(logger, step)
+
+    def update(self, replay_buffer, logger, step):
+        obs, action, extr_reward, next_obs, not_done = replay_buffer.sample(
+            self.batch_size)
+        logger.log('train/batch_reward', extr_reward.mean(), step)
+        
+        obs, obs_aug = self.augmentation(obs)
+        next_obs, next_obs_aug = self.augmentation(next_obs)
+        if self.reward_free : 
+            self.update_representation(obs, logger, step)
+            intr_reward = self.compute_reward(next_obs).detach()
+            logger.log('train/intr_reward', intr_reward.mean(), step)
+            reward = intr_reward
+        else :
+            reward = extr_reward
+        self.update_critic(obs, obs_aug, action, reward, next_obs,
+                           next_obs_aug, not_done, logger, step)
+
+        if step % self.actor_update_frequency == 0:
+            self.update_actor_and_alpha(obs, logger, step)
+
+        if step % self.critic_target_update_frequency == 0:
+            utils.soft_update_params(self.critic, self.critic_target,
+                                     self.critic_tau)
+
---
+++ utils.py
@@ -0,0 +1,233 @@
+import math
+import os
+import random
+from collections import deque
+
+import numpy as np
+import scipy.linalg as sp_la
+
+import gym
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from skimage.util.shape import view_as_windows
+from torch import distributions as pyd
+
+
+class eval_mode(object):
+    def __init__(self, *models):
+        self.models = models
+
+    def __enter__(self):
+        self.prev_states = []
+        for model in self.models:
+            self.prev_states.append(model.training)
+            model.train(False)
+
+    def __exit__(self, *args):
+        for model, state in zip(self.models, self.prev_states):
+            model.train(state)
+        return False
+
+
+def soft_update_params(net, target_net, tau):
+    for param, target_param in zip(net.parameters(), target_net.parameters()):
+        target_param.data.copy_(tau * param.data +
+                                (1 - tau) * target_param.data)
+
+
+def set_seed_everywhere(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def make_dir(*path_parts):
+    dir_path = os.path.join(*path_parts)
+    try:
+        os.mkdir(dir_path)
+    except OSError:
+        pass
+    return dir_path
+
+
+def tie_weights(src, trg):
+    assert type(src) == type(trg)
+    trg.weight = src.weight
+    trg.bias = src.bias
+
+
+def weight_init(m):
+    """Custom weight init for Conv2D and Linear layers."""
+    if isinstance(m, nn.Linear):
+        nn.init.orthogonal_(m.weight.data)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.orthogonal_(m.weight.data, gain)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0.0)
+
+
+def mlp(input_dim, hidden_dim, output_dim, hidden_depth, output_mod=None):
+    if hidden_depth == 0:
+        mods = [nn.Linear(input_dim, output_dim)]
+    else:
+        mods = [nn.Linear(input_dim, hidden_dim), nn.ReLU(inplace=True)]
+        for i in range(hidden_depth - 1):
+            mods += [nn.Linear(hidden_dim, hidden_dim), nn.ReLU(inplace=True)]
+        mods.append(nn.Linear(hidden_dim, output_dim))
+    if output_mod is not None:
+        mods.append(output_mod)
+    trunk = nn.Sequential(*mods)
+    return trunk
+
+
+def to_np(t):
+    if t is None:
+        return None
+    elif t.nelement() == 0:
+        return np.array([])
+    else:
+        return t.cpu().detach().numpy()
+
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, k):
+        gym.Wrapper.__init__(self, env)
+        self._k = k
+        self._frames = deque([], maxlen=k)
+        shp = env.observation_space.shape
+        self.observation_space = gym.spaces.Box(
+            low=0,
+            high=1,
+            shape=((shp[0] * k,) + shp[1:]),
+            dtype=env.observation_space.dtype)
+        self._max_episode_steps = env._max_episode_steps
+
+    def reset(self):
+        obs = self.env.reset()
+        for _ in range(self._k):
+            self._frames.append(obs)
+        return self._get_obs()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self._frames.append(obs)
+        return self._get_obs(), reward, done, info
+
+    def _get_obs(self):
+        assert len(self._frames) == self._k
+        return np.concatenate(list(self._frames), axis=0)
+
+
+class TanhTransform(pyd.transforms.Transform):
+    domain = pyd.constraints.real
+    codomain = pyd.constraints.interval(-1.0, 1.0)
+    bijective = True
+    sign = +1
+
+    def __init__(self, cache_size=1):
+        super().__init__(cache_size=cache_size)
+
+    @staticmethod
+    def atanh(x):
+        return 0.5 * (x.log1p() - (-x).log1p())
+
+    def __eq__(self, other):
+        return isinstance(other, TanhTransform)
+
+    def _call(self, x):
+        return x.tanh()
+
+    def _inverse(self, y):
+        # We do not clamp to the boundary here as it may degrade the performance of certain algorithms.
+        # one should use `cache_size=1` instead
+        return self.atanh(y)
+
+    def log_abs_det_jacobian(self, x, y):
+        # We use a formula that is more numerically stable, see details in the following link
+        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
+        return 2. * (math.log(2.) - x - F.softplus(-2. * x))
+
+
+class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
+    def __init__(self, loc, scale):
+        self.loc = loc
+        self.scale = scale
+
+        self.base_dist = pyd.Normal(loc, scale)
+        transforms = [TanhTransform()]
+        super().__init__(self.base_dist, transforms)
+
+    @property
+    def mean(self):
+        mu = self.loc
+        for tr in self.transforms:
+            mu = tr(mu)
+        return mu
+class RMS(object):
+    """running mean and std """
+    def __init__(self, device, epsilon=1e-4, shape=(1,)):
+        self.M = torch.zeros(shape).to(device)
+        self.S = torch.ones(shape).to(device)
+        self.n = epsilon
+
+    def __call__(self, x):
+        bs = x.size(0)
+        delta = torch.mean(x, dim=0) - self.M
+        new_M = self.M + delta * bs / (self.n + bs)
+        new_S = (self.S * self.n + torch.var(x, dim=0) * bs +
+                 torch.square(delta) * self.n * bs /
+                 (self.n + bs)) / (self.n + bs)
+
+        self.M = new_M
+        self.S = new_S
+        self.n += bs
+
+        return self.M, self.S
+
+class PBE(object):
+    """particle-based entropy based on knn normalized by running mean """
+    def __init__(self, rms, knn_clip, knn_k, knn_avg, knn_rms, device):
+        self.rms = rms
+        self.knn_rms = knn_rms
+        self.knn_k = knn_k
+        self.knn_avg = knn_avg
+        self.knn_clip = knn_clip
+        self.device = device
+
+    def __call__(self, rep):
+        source = target = rep
+        b1, b2 = source.size(0), target.size(0)
+        # (b1, 1, c) - (1, b2, c) -> (b1, 1, c) - (1, b2, c) -> (b1, b2, c) -> (b1, b2)
+        sim_matrix = torch.norm(source[:, None, :].view(b1, 1, -1) -
+                                target[None, :, :].view(1, b2, -1),
+                                dim=-1,
+                                p=2)
+        reward, _ = sim_matrix.topk(self.knn_k,
+                                    dim=1,
+                                    largest=False,
+                                    sorted=True)  # (b1, k)
+        if not self.knn_avg:  # only keep k-th nearest neighbor
+            reward = reward[:, -1]
+            reward = reward.reshape(-1, 1)  # (b1, 1)
+            reward /= self.rms(reward)[0] if self.knn_rms else 1.0
+            reward = torch.maximum(
+                reward - self.knn_clip,
+                torch.zeros_like(reward).to(self.device)
+            ) if self.knn_clip >= 0.0 else reward  # (b1, 1)
+        else:  # average over all k nearest neighbors
+            reward = reward.reshape(-1, 1)  # (b1 * k, 1)
+            reward /= self.rms(reward)[0] if self.knn_rms else 1.0
+            reward = torch.maximum(
+                reward - self.knn_clip,
+                torch.zeros_like(reward).to(
+                    self.device)) if self.knn_clip >= 0.0 else reward
+            reward = reward.reshape((b1, self.knn_k))  # (b1, k)
+            reward = reward.mean(dim=1, keepdim=True)  # (b1, 1)
+        reward = torch.log(reward + 1.0)
+        return reward
